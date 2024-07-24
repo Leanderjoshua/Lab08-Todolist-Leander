4559731b8a901ac28de5687ec1563c4470cd3459
Leanderjoshua <lvijayak@uab.edu>
2024-07-24 14:48:02 -0500

    To do list application of Leander

---
+++ Lab08/lab08/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Default styling, can be left empty or customized
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
